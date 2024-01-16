cee6a5f96371db3876a7ab7602304e2ad12125c3
ILTACK-OH <dlfxor0329@khu.ac.kr>
2024-01-16 20:34:23 +0900

    hangman game v3

--- HangMan/main.py
+++ HangMan/main.py
@@ -8,21 +8,31 @@ chosen_word = mrw()
 answer_check = ["_"] * len(chosen_word)
 
 print(logo)
+print("Enter only one letter!")
 print("".join(answer_check))  # 선택된 단어의 길이와 같은 길이의 리스트 생성
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()  # 사용자 입력
-    answer_check= make_answer(chosen_word, answer_check, guess=guess)  # 입력 문자가 단어에 있으면 display변환
+    if guess in answer_check:
+        print(f"You've already guessed {guess}")
+        print(display)
+        print(stages[lives])
+        continue
+    
+    answer_check= make_answer(chosen_word, answer_check, guess=guess)  # 게임 진행 저장 리스트
     display = "".join(answer_check)
-    print(display)
+    # 추측한 문자가 단어에 없을 경우
     if guess not in chosen_word:
         lives -= 1
-    
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+
+    print(display)
     print(stages[lives])
+    # 목숨이 0이 되면 게임 종료(패배)
     if lives == 0:
         end_of_game = True
         print("You lose...")
-    
+    # 단어를 맞추면 게임 종료(승리)
     if display == chosen_word:
         end_of_game = True
         print("You win!!!")
--- HangMan/utils/make_word.py
+++ HangMan/utils/make_word.py
@@ -1,10 +1,7 @@
 import random
+from utils.word_list import word_list 
+
 # word_list의 단어를 무작위로 선택하는 함수
-
-# 두 개의 경우 함수가 호출될때마다 참조가 발생하는 것보다 전역으로 사용하는 것이 좋다고 생각하였음.
-word_list = ["aardvark", "baboon", "camel"]
-len_word = len(word_list)
-
 def make_random_word():
     chosen_word = random.choice(word_list)
     return chosen_word
